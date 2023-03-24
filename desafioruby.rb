bda18c425694f66446d0dd9cba1d478737fb2e65
diogenesprenhaca01 <110768106+diogenesprenhaca01@users.noreply.github.com>
2023-03-24 18:44:12 -0300

    codigo em ruby
    codigo em ruby para entrada e saida de dados, coleta de nome sobrenome e idade do usuario

---
+++ desafioruby.rb
@@ -0,0 +1,15 @@
+#programa simples para leitura de nome e sobre nome do usuario e sua idade
+
+puts "Seu nome ?"
+
+nome = gets.chomp
+
+puts "Seu sobrenome ?"
+
+sobrenome = gets.chomp
+
+puts "Sua idade ?"
+
+idade = gets.chomp
+
+puts " #{nome} #{sobrenome} #{idade} anos."
